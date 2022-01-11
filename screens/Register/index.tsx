a6b36e11747692fa9f49e1e1c61263c0875981a8
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2022-01-11 17:09:12 -0400

    initial register layout added

--- screens/Register/index.tsx
+++ screens/Register/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
 import registerUser from '../../packages/actions/registerUser';
+
+import { TextInput } from '@bits-x/text-input';
+import { Button } from '@bits-x/button';
 
 type Props = typeof dispatch;
 
@@ -11,11 +14,33 @@ const RegisterScreen = ({ register }: Props) => {
     register({ email: 'hw@gmail.com', username: 'howie' });
   }, [register]);
 
-  return <Text>Register</Text>;
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Regístrate</Text>
+        <TextInput placeholder="Nombre de usuario" />
+        <TextInput placeholder="Correo eletrónico" />
+        <TextInput placeholder="Contraseña" />
+      </View>
+      <Button>Crear Cuenta</Button>
+    </View>
+  );
 };
 
 const dispatch = {
   register: registerUser.request,
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 24,
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    marginBottom: 12,
+  },
+});
 
 export default connect(null, dispatch)(RegisterScreen);
